90395da41cdcb0f910f930f66648f4929fc88d5f
schillerk <kyle@Kyles-MacBook-Pro-4.local>
2018-07-30 14:17:46 -0700

    refactor

--- app.py
+++ app.py
@@ -10,51 +10,13 @@ import ast
 app = Flask(__name__)
 CORS(app)
 
-# connection = sqlite3.connect('data.db')
-# cursor = connection.cursor()
-
-# create_table = 'CREATE TABLE ideas (blob text)'
-# cursor.execute(create_table)
-
-# connection.commit()
-# connection.close()
-
-# field = 'Gender'
-# field2 = 'Count'
-# print sum(df.loc[(df['Race/Ethnicity'] == 'White') & (df['Gender'] == 'Male') & df['Count'] > 0, 'Count'])
-# print df.loc[(df[field] == 'Male') & df[field2] > 0]
-# print df.query('''Gender == 'Male' & RE == 'White' & Count > 0''')
-# print df[df.Location == "USA" & df.Gender == "Male"]
-# print df[df.Gender == "Male" & df.Location == "USA"]
-
-# print str(int(sum(df.query(buildQuery(test_query))['Count'])))
-
-def buildQuery(fields):
-	print ' & '.join(['{} == \'{}\''.format(k, v) for k, v in fields.items()]) + ' & Count > 0'
-	out = ''
-	for k, v in fields.items():
-		if v != 'All':
-			out += '{} == \'{}\''.format(k, v) + ' & '
-	print out + 'Count > 0'
-	return out + 'Count > 0'
-
-def run_query(fields):
-	return df.query(buildQuery(fields))
-
-def parseQuery(fields):
-	results = run_query(fields)['Count']
-	total = sum(results)
-	return str(int(total))
-
 @app.route('/')
-def fun():
+def home():
 	return render_template('index.html')
-	# return jsonify(data)
 
 @app.route('/submit/<string:idea>')
 def submit_idea(idea):
 	print idea
-	# print ast.literal_eval(idea)
 	connection = sqlite3.connect('data.db')
 	cursor = connection.cursor()
 
@@ -64,32 +26,6 @@ def submit_idea(idea):
 	connection.commit()
 	connection.close()
 	return ''
-
-@app.route('/total')
-def total():
-	return str(int(sum(df.loc[(df['Count'] > 0), 'Count'])))
-
-@app.route('/query/<string:q>')
-def return_query_results(q):
-	print q
-	return parseQuery(ast.literal_eval(q))
-
-@app.route('/getjsondata')
-def getjson():
-	return jsonify(data)
-
-@app.route('/test')
-def test():
-	connection = sqlite3.connect('data.db')
-	cursor = connection.cursor()
-
-	test_company = (1, 'Zenysis')
-	insert_query = 'INSERT INTO companies VALUES (?, ?)'
-	cursor.execute(insert_query, test_company)
-
-	connection.commit()
-	connection.close()
-	return 'hello worlddadsf'
 
 @app.route('/getdata')
 def getdata():
@@ -105,13 +41,11 @@ def getdata():
 	connection.close()
 	return jsonify(out)
 
-@app.route('/callAPI')
-def call():
-	# make an api call
-	# print 'testing call'
+@app.route('/textbeltdemo')
+def textbelt():
 	requests.post('https://textbelt.com/text', {
-	  'phone': '6503324607',
-	  'message': 'Hello fuck twilio',
-	  'key': TEXTBELT_KEY,
+	  'phone': 'PHONE NUMBER GOES HERE',
+	  'message': 'Hello World! Twilio sucks.',
+	  'key': 'TEXTBELT KEY GOES HERE',
 	})
 	return ''
---
+++ generate_db.py
@@ -0,0 +1,10 @@
+# Initialize Datbase
+
+# connection = sqlite3.connect('data.db')
+# cursor = connection.cursor()
+
+# create_table = 'CREATE TABLE ideas (blob text)'
+# cursor.execute(create_table)
+
+# connection.commit()
+# connection.close()
